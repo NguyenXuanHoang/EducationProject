55886faa42c02a79073590efa522c11b2b49d311
HoangNX1503 <hoangnx1503@gmail.com>
2019-06-20 16:51:32 +0700

    -change log config and try catch connection

--- DuAnGiaoDuc/src/main/java/vn/iomedia/ipay/connection/SQLConnection.java
+++ DuAnGiaoDuc/src/main/java/vn/iomedia/ipay/connection/SQLConnection.java
@@ -4,18 +4,22 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class SQLConnection {
 
+    private static Log log = LogFactory.getLog(SQLConnection.class);
+
     public static EntityManager getConnection() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("tuyensinh");
-        
-        EntityManager em = emf.createEntityManager();
-        return em;
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("tuyensinh");
+            EntityManager em = emf.createEntityManager();
+            return em;
+        } catch (Exception exp) {
+            log.error(exp.getMessage());
+            return null;
+        }
     }
 
-//    public static void closeConnection(EntityManager em) {
-//        if (em != null) {
-//            em.close();
-//        }
-//    }
 }
